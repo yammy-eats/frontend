d32358652c0921b9f0e4bb6dedd2c69e315f29ff
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-26 10:59:02 +0900

    feat: setup apollo graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles/styles.css"
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+} from "@apollo/client";
+import {client} from "./apollo";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>,
   </React.StrictMode>
 );
 

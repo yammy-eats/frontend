626f1769fa0d79e8c3355f17bdccacb9e01925ff
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-28 14:41:41 +0900

    feat: if login input is invalid, can't click button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,8 @@ import {FormError} from "../components/form-error";
 import {gql, useMutation} from "@apollo/client";
 import {loginMutation, loginMutationVariables} from "../__generated__/loginMutation";
 import nuberLogo from "../images/logo.svg"
+import {Button} from "../components/button";
+import {Link} from "react-router-dom";
 
 const LOGIN_MUTATION = gql`
     mutation loginMutation($loginInput: LoginInput!) {
@@ -22,7 +24,7 @@ interface ILoginForm {
 }
 
 export const Login = () => {
-    const {register, getValues, formState: {errors}, handleSubmit} = useForm<ILoginForm>();
+    const {register, getValues, formState: {errors, isValid}, handleSubmit} = useForm<ILoginForm>( {mode:"onChange"});
 
     const onCompleted = (data: loginMutation) => {
         const {login: {ok, token}} = data;
@@ -58,7 +60,7 @@ export const Login = () => {
             <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
                 <img src={nuberLogo} className="w-52 mb-10"/>
                 <h4 className="w-full text-3xl mb-5">Welcome back</h4>
-                <form onSubmit={handleSubmit(onSubmit)} className="w-full grid gap-3 mt-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="w-full grid gap-3 mt-5 mb-5">
                     <input {...register("email", {
                         required: "Email을 입력해주세요",
                         pattern: /^[A-Za-z0-9._%+-]+@+[a-z.+]+\.+[a-z]+$/,
@@ -74,11 +76,12 @@ export const Login = () => {
                            placeholder="Password" className="input"/>
                     {errors.password?.type === "minLength" && (<FormError errorMessage="비밀번호는 8자이상 입력해주세요"/>)}
 
-                    <button className="btn">
-                        {loading ? "Loding..." : "Log In"}
-                    </button>
+                    <Button canClick={isValid} loading={loading} actionText="Log In"/>
                     {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult?.login.error}/>}
                 </form>
+                <div>
+                    New to Yammy? <Link to={"/create-account"} className="text-lime-600 hover:underline">Create an Account</Link>
+                </div>
             </div>
         </div>
     )

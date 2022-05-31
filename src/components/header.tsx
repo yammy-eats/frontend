aaba55c29b9421f30d374909b10567e7e4a0b3b8
HayanYoo <hayanyoo.dev@gmail.com>
2022-05-31 17:57:16 +0900

    modify: gitignore에 style.css 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
     const {data} = useMe()
     return (
         <header className="py-4">
-            <div className="w-full px-5 2xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
+            <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
                 <img src={nuberLogo} className="w-28"/>
                 <span className="text-xs">
                     <Link to="/my-profile">
